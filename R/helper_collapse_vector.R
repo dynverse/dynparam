6a3f55db8975b3659a0add560835482c8941add8
Robrecht Cannoodt <rcannood@gmail.com>
2018-11-26 11:34:25 +0100

    added helper function

---
+++ R/helper_collapse_vector.R
@@ -0,0 +1,10 @@
+#' A helper function for collapsing a vector
+#'
+#' Will surround the collapsed vector with brackets if it has more than one element.
+collapse_vector <- function(vec) {
+  if (length(vec) == 1) {
+    vec
+  } else {
+    paste0("{", paste(vec, collapse = ", "), "}")
+  }
+}
