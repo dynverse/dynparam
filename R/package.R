c50f74669e1ecef281bfac3689e5affbf6e14caf
Robrecht Cannoodt <rcannood@gmail.com>
2018-11-26 11:34:33 +0100

    added package documentation

--- R/package.R
+++ R/package.R
@@ -1,15 +1,32 @@
-#' Helper package for parameter stuff
+#' Wrapper functions for descripting parameters
 #'
 #' @import dplyr
 #' @import tidyr
-#' @import methods
 #' @import tibble
 #' @import dynutils
-#' @import readr
 #' @import purrr
 #' @importFrom magrittr %<>% %$% set_rownames set_colnames
 #' @importFrom glue glue
 #'
 #' @docType package
 #' @name dynparam
+#'
+#' @section Parameters:
+#' These functions help you provide a meta description of parameters.
+#'
+#' Implemented are the following functions:
+#' * [character_parameter()]
+#' * [integer_parameter()]
+#' * [logical_parameter()]
+#' * [numeric_parameter()]
+#'
+#' See [?parameters][dynparam::parameters] for a list of helper functions converting parameters from and to other formats.
+#'
+#' @section Distributions:
+#' These distributions allow to define prior distributions for numeric and integer parameters.
+#'
+#' Implemented are the following distributions:
+#' * [uniform_distribution()]
+#' * [expuniform_distribution()]
+#' * [normal_distribution()]
 NULL
