f6b04c1df6f710b0e04b2858b709d8dc238edc3f
Robrecht Cannoodt <rcannood@gmail.com>
2018-11-27 14:39:14 +0100

    fun with functions and formulae

--- tests/testthat/test-helper_check_finite.R
+++ tests/testthat/test-helper_check_finite.R
@@ -13,4 +13,6 @@ test_that("check_finite works", {
   expect_false(check_finite(NULL))
   expect_false(check_finite(c(1,2)))
   expect_false(check_finite(1:10))
+  expect_false(check_finite(~ help_me + im_trapped + in_a_unit_test))
+  expect_false(check_finite(function(x) "call 911 now"))
 })
