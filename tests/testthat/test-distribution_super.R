6e75713a9849695b573323c2c0f8999eb2293492
Robrecht Cannoodt <rcannood@gmail.com>
2018-11-28 14:05:57 +0100

    expand distribution tests

--- tests/testthat/test-distribution_super.R
+++ tests/testthat/test-distribution_super.R
@@ -12,3 +12,17 @@ test_that("as_distribution works", {
   expect_equal(as_distribution(list(class = "normal_distribution", mean = 1L, sd = 2L)), normal_distribution(mean = 1L, sd = 2L))
   expect_equal(as_distribution(list(class = "normal_distribution", mean = 1L, sd = 2L, lower = -4L, upper = 3L)), normal_distribution(mean = 1L, sd = 2L, lower = -4L, upper = 3L))
 })
+
+test_that("is_distribution works", {
+  dis <- distribution(a = 1, b = 2)
+  expect_true(is_distribution(dis))
+
+  lis <- list(a = 1, b = 2)
+  expect_false(is_distribution(lis))
+})
+
+test_that("print and cat works", {
+  dis <- distribution(a = 1, b = 2)
+  expect_equal(as.character(dis), "distribution(a = 1, b = 2)")
+  expect_output(print(dis), "distribution\\(a = 1, b = 2\\)")
+})
