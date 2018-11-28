da9bd0f8029d7d36b8398e8fb70ca1af8d0c1f2c
Robrecht Cannoodt <rcannood@gmail.com>
2018-11-28 14:35:18 +0100

    fix list wrapping in numeric vector

--- R/param_numeric.R
+++ R/param_numeric.R
@@ -54,7 +54,7 @@ as_paramhelper.numeric_parameter <- function(param) {
     trafo = qfun
   )
   if (length != 1) args$len <- length
-  list(params = do.call(fun, args))
+  list(params = list(do.call(fun, args)))
 }
 
 #' @export
