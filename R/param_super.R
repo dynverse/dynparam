29d5ddf189dd318b455f29c3977da7aa64cafd34
Wouter Saelens <wouter.saelens@gmail.com>
2019-04-12 11:44:12 +0200

    defenestrate Hmisc

--- R/param_super.R
+++ R/param_super.R
@@ -154,7 +154,6 @@ on_failure(is_parameter) <- function(call, env) {
 #' @param x The parameter
 #' @param sep A separator between different fields
 #'
-#' @importFrom Hmisc capitalize
 #' @importFrom stringr str_replace_all str_replace str_glue_data
 #'
 #' @export
@@ -167,7 +166,7 @@ get_description <- function(
   description <-
     (x$description %||% "") %>%                     # use "" if no description is provided
     str_replace_all("\n", "") %>%                   # remove newlines
-    Hmisc::capitalize() %>%                         # capitalise sentences
+    capitalise() %>%                         # capitalise sentences
     str_replace_all("\\\\link\\[[a-zA-Z0-9_:]*\\]\\{([^\\}]*)\\}", "\\1") %>%  # substitute \link[X](Y) with just Y
     str_replace_all("[ \t\\.]*$", "")               # remove trailing whitespace and punctuation
 
@@ -180,7 +179,7 @@ get_description <- function(
     lis[!names(lis) %in% c("id", "type")] %>%
     as.list() %>%
     stringr::str_glue_data("{names(.)}: {.}") %>%
-    Hmisc::capitalize() %>%
+    capitalise() %>%
     paste0(collapse = sep)
 
   paste0(description, sep, extra_text)
@@ -200,4 +199,12 @@ as_descriptive_tibble.parameter <- function(x) {
     domain = NA,
     default = collapse_set(x$default)
   )
+}
+
+
+
+capitalise <- function(string) {
+  capped <- grep("^[A-Z]", string, invert = TRUE)
+  substr(string[capped], 1, 1) <- toupper(substr(string[capped], 1, 1))
+  return(string)
 }
